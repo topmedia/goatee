391d56c4c39dca2f1f26a1bcef59e47434af58a1
Patrick Lenz <patrick@limited-overload.de>
2015-01-08 00:07:57 +0100

    Reorganize code

--- main.go
+++ main.go
@@ -26,76 +26,81 @@ type Config struct {
 	Destination string
 }
 
-func main() {
-	var config Config
-	if _, err := toml.DecodeFile("goatee.cfg", &config); err != nil {
-		log.Fatalf("Error opening config file: %s", err)
-	}
+type Goatee struct {
+	config Config
+	client *imap.Client
+}
 
+func (g *Goatee) Connect() {
+	var err error
 	log.Print("Connecting to server..\n")
-	c, err := imap.DialTLS(config.Server, &tls.Config{})
+	g.client, err = imap.DialTLS(g.config.Server, &tls.Config{})
 
 	if err != nil {
 		log.Fatalf("Connection to server failed: %s", err)
 	}
 
-	defer c.Logout(30 * time.Second)
-
-	if c.State() == imap.Login {
+	if g.client.State() == imap.Login {
 		log.Print("Logging in..\n")
-		c.Login(config.User, config.Password)
+		g.client.Login(g.config.User, g.config.Password)
 	}
 
 	log.Print("Opening INBOX..\n")
-	c.Select("INBOX", true)
+	g.client.Select("INBOX", true)
+}
 
+func (g *Goatee) ExtractAttachment(msg *mail.Message, params map[string]string) {
+	mr := multipart.NewReader(msg.Body, params["boundary"])
+	for {
+		p, err := mr.NextPart()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			log.Fatalf("Error parsing part: %s", err)
+		}
+
+		ct := p.Header.Get("Content-Type")
+		if strings.HasPrefix(ct, "application/pdf") {
+			path := filepath.Join(".", g.config.Destination,
+				p.FileName())
+			dst, err := os.Create(path)
+			if err != nil {
+				log.Fatalf("Failed to create file: %s", err)
+			}
+			r := base64.NewDecoder(base64.StdEncoding, p)
+			_, err = io.Copy(dst, r)
+			if err != nil {
+				log.Fatalf("Failed to store attachment: %s", err)
+			}
+		}
+	}
+}
+
+func (g *Goatee) FetchMails() {
 	set, _ := imap.NewSeqSet("1:*")
 	log.Print("Fetching mails..\n")
-	cmd, err := c.Fetch(set, "BODY[]")
+	cmd, err := g.client.Fetch(set, "BODY[]")
 
 	if err != nil {
 		log.Fatalf("Fetch failed: %s", err)
 	}
 
 	for cmd.InProgress() {
-		c.Recv(10 * time.Second)
+		g.client.Recv(10 * time.Second)
 
 		for _, rsp := range cmd.Data {
-			header := imap.AsBytes(rsp.MessageInfo().Attrs["BODY[]"])
+			body := imap.AsBytes(rsp.MessageInfo().Attrs["BODY[]"])
 
-			if msg, _ := mail.ReadMessage(bytes.NewReader(header)); msg != nil {
+			if msg, _ := mail.ReadMessage(bytes.NewReader(body)); msg != nil {
 				fmt.Println("|--", msg.Header.Get("Subject"))
-				mediaType, params, _ := mime.ParseMediaType(msg.Header.Get("Content-Type"))
+				mediaType, params, _ := mime.ParseMediaType(
+					msg.Header.Get("Content-Type"))
 				if strings.HasPrefix(mediaType, "multipart/") {
-					mr := multipart.NewReader(msg.Body, params["boundary"])
-					for {
-						p, err := mr.NextPart()
-						if err == io.EOF {
-							break
-						} else if err != nil {
-							log.Fatalf("Error parsing part: %s", err)
-						}
-
-						ct := p.Header.Get("Content-Type")
-						if strings.HasPrefix(ct, "application/pdf") {
-							path := filepath.Join(".", config.Destination,
-								p.FileName())
-							dst, err := os.Create(path)
-							if err != nil {
-								log.Fatalf("Failed to create file: %s", err)
-							}
-							r := base64.NewDecoder(base64.StdEncoding, p)
-							_, err = io.Copy(dst, r)
-							if err != nil {
-								log.Fatalf("Failed to store attachment: %s", err)
-							}
-						}
-					}
+					g.ExtractAttachment(msg, params)
 				}
 			}
 		}
 		cmd.Data = nil
-		c.Data = nil
 	}
 
 	if rsp, err := cmd.Result(imap.OK); err != nil {
@@ -105,5 +110,18 @@ func main() {
 			log.Fatalf("Fetch error:", rsp.Info)
 		}
 	}
+}
 
+func (g *Goatee) ReadConfig(filename string) {
+	if _, err := toml.DecodeFile(filename, &g.config); err != nil {
+		log.Fatalf("Error opening config file: %s", err)
+	}
+}
+
+func main() {
+	g := Goatee{}
+	g.ReadConfig("goatee.cfg")
+	g.Connect()
+	defer g.client.Logout(30 * time.Second)
+	g.FetchMails()
 }
